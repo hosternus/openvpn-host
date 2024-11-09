58f1a838b13ae475f1cdbc69182a63c3c73c1b6c
hosternus <90009071+hosternus@users.noreply.github.com>
2024-11-09 17:17:13 +0300

    Update enable-ip-forwarding.sh

--- enable-ip-forwarding.sh
+++ enable-ip-forwarding.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Включение IP-forwarding
 echo "Включаем IP-forwarding..."
 sysctl -w net.ipv4.ip_forward=1
@@ -7,6 +8,11 @@ sysctl -w net.ipv4.ip_forward=1
 # Применение изменений sysctl
 sysctl -p
 
+
+# Установка пакета iptables-persistent для сохранения правил
+echo "Устанавливаем iptables-persistent для сохранения правил NAT..."
+apt-get update -y
+apt-get install -y iptables-persistent
 
 # Определение сетевого интерфейса, через который сервер подключен к Интернету
 INTERFACE=$(ip route | grep default | awk '{print $5}')
@@ -16,13 +22,9 @@ echo "Используем интерфейс: $INTERFACE"
 echo "Настраиваем NAT с использованием iptables..."
 iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o "$INTERFACE" -j MASQUERADE
 
-# Установка пакета iptables-persistent для сохранения правил
-echo "Устанавливаем iptables-persistent для сохранения правил NAT..."
-apt-get update -y
-apt-get install -y iptables-persistent
-
 # Сохранение правил iptables для загрузки при старте системы
 echo "Сохраняем правила iptables..."
 iptables-save > /etc/iptables/rules.v4
+
 
 echo "Настройка завершена! IP-forwarding включен, NAT настроен и сохранен."
