fcf287426c37f57f8abfe8f82891b22c73b5d6b1
hosternus <90009071+hosternus@users.noreply.github.com>
2024-11-11 12:08:20 +0300

    automaintenance update (ip-fwd and openvpn)

--- automaintenance.sh
+++ automaintenance.sh
@@ -1,27 +1,49 @@
 #!/bin/bash
 
-# Запрос пути к скрипту обслуживания
-read -p "Введите полный путь к скрипту обслуживания (например, /path/to/maintenance.sh): " MAINTENANCE_SCRIPT
+# Запрос пути к каталогу исходников сервера
+read -p "Введите полный путь к каталогу с исходными файлами библиотеки (например, /path/to/openvpn-host): " MAINTENANCE_SCRIPT
 
-# Проверка, существует ли указанный файл
-if [ ! -f "$MAINTENANCE_SCRIPT" ]; then
-    echo "Скрипт обслуживания не найден по указанному пути: $MAINTENANCE_SCRIPT."
+
+# Проверка, существует ли указанные файлы
+if [ ! -f "$MAINTENANCE_SCRIPT/maintenance.sh" ]; then
+    echo "Скрипт обслуживания openvpn не найден по указанному пути: $MAINTENANCE_SCRIPT/maintenance.sh."
+    exit 1
+fi
+if [ ! -f "$MAINTENANCE_SCRIPT/enable-ip-forwarding.sh" ]; then
+    echo "Скрипт обслуживания ip-forwarding не найден по указанному пути: $MAINTENANCE_SCRIPT/enable-ip-forwarding.sh."
     exit 1
 fi
 
+
+# Maintenance
 # Лог-файл для записи результатов работы скрипта обслуживания
 LOG_FILE="/var/log/openvpn_maintenance.log"
 
 # Проверка, добавлено ли уже задание для данного скрипта в cron
-if crontab -l 2>/dev/null | grep -Fq "$MAINTENANCE_SCRIPT"; then
+if crontab -l 2>/dev/null | grep -Fq "$MAINTENANCE_SCRIPT/maintenance.sh"; then
     echo "Задание для обслуживания уже добавлено в cron."
 else
     echo "Добавление задания для обслуживания в cron..."
 
     # Добавление задания в cron для ежедневного запуска в 3:00
-    (crontab -l 2>/dev/null; echo "0 3 * * * $MAINTENANCE_SCRIPT > $LOG_FILE 2>&1") | crontab -
+    (crontab -l 2>/dev/null; echo "0 3 * * * $MAINTENANCE_SCRIPT/maintenance.sh > $LOG_FILE 2>&1") | crontab -
 
     echo "Задание для обслуживания добавлено в cron."
 fi
+
+
+# IP-forwarding
+# Проверка, добавлено ли уже задание для данного скрипта в cron
+if crontab -l 2>/dev/null | grep -Fq "$MAINTENANCE_SCRIPT/enable-ip-forwarding.sh"; then
+    echo "Задание для обслуживания уже добавлено в cron."
+else
+    echo "Добавление задания для обслуживания в cron..."
+
+    # Добавление задания в cron для ежедневного запуска в 3:00
+    (crontab -l 2>/dev/null; echo "0 3 * * * $MAINTENANCE_SCRIPT/enable-ip-forwarding.sh > $LOG_FILE 2>&1") | crontab -
+
+    echo "Задание для обслуживания добавлено в cron."
+fi
+
 
 echo "Проверка завершена."
