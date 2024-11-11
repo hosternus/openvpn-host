fe79e96800855754d3e72bbb71fe11ffce8037c2
hosternus <90009071+hosternus@users.noreply.github.com>
2024-11-11 11:49:30 +0300

    clear ipt before install

--- enable-ip-forwarding.sh
+++ enable-ip-forwarding.sh
@@ -17,6 +17,10 @@ apt-get install -y iptables-persistent iptables
 INTERFACE=$(ip route | grep default | awk '{print $5}')
 echo "Используем интерфейс: $INTERFACE"
 
+# Сброс прошлой конфигурации iptables
+rm -rf /etc/iptables/rules.v4
+iptables -F
+
 # Настройка NAT для выхода трафика в Интернет
 echo "Настраиваем NAT с использованием iptables..."
 iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o "$INTERFACE" -j MASQUERADE
